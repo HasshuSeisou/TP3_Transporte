1d5ddf3680fb2aaf1f114842faf288549ff6a51b
HasshuSeisou <hashv2.2@gmail.com>
2024-07-01 13:04:36 -0300

    añadido ell codigo del server esta vez de verdad

---
+++ server.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('localhost',12345))
+s.listen()
+
+conn, addr = s.accept()
+data = conn.recv(1024)
+conn.sendall("Holis soy er".encode())
+print(data)
